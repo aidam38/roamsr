ee1270bb309a34ca91e42c423028723cbc6e820d
luccr1 <lucca.hellriegel@gmx.net>
2021-03-10 10:35:17 +0100

    refactoring: keybindings are handled via map-based dispatch based on status and key-code

--- js/stable/keybindings.js
+++ js/stable/keybindings.js
@@ -3,62 +3,76 @@ import { endSession, getCurrentCard } from "./sessions";
 import { showAnswerAndCloze } from "./styles";
 import { addResponseButtons } from "./uiElements";
 
-// note: changing these requires reloading Roam because of the keylistener
-export const processKey = (e) => {
-	// console.log("alt: " + e.altKey + "  shift: " + e.shiftKey + "  ctrl: " + e.ctrlKey + "   code: " + e.code + "   key: " + e.key);
-	if (document.activeElement.type == "textarea" || !location.href.includes(getCurrentCard().uid)) {
-		return;
-	}
+const reviewAndTestCodeMap = {
+	KeyF: flagCard,
+	KeyS: stepToNext,
+	KeyD: (e) => {
+		// TODO: this does not work in any version because alt+d ist opening the daily page
+		if (e.altKey) endSession();
+	},
+};
 
-	var responses = getCurrentCard().algorithm(getCurrentCard().history);
-	var handleNthResponse = async (n) => {
-		console.log("Handling response: " + n);
-		if (n >= 0 && n < responses.length) {
-			const res = responses[n];
-			if (res.interval != 0) {
-				responseHandler(getCurrentCard(), res.interval, res.signal.toString());
-			} else {
-				await responseHandler(getCurrentCard(), res.interval, res.signal.toString());
-			}
-			stepToNext();
-		}
-	};
-
-	// Bindings for 123456789
-	if (e.code.includes("Digit")) {
-		var n = Math.min(parseInt(e.code.replace("Digit", "")) - 1, responses.length - 1);
-		handleNthResponse(n);
-		return;
-	}
-
-	// Bindings for hjkl
-	const letters = ["KeyH", "KeyJ", "KeyK", "KeyL"];
-	if (letters.includes(e.code)) {
-		var n = Math.min(letters.indexOf(e.code), responses.length - 1);
-		handleNthResponse(n);
-		return;
-	}
-
-	if (e.code == "Space") {
+const reviewCodeMap = {
+	Space: () => {
 		showAnswerAndCloze();
 		addResponseButtons();
-		return;
-	}
+	},
+	...reviewAndTestCodeMap,
+};
 
-	if (e.code == "KeyF") {
-		flagCard();
-		return;
-	}
-
-	if (e.code == "KeyS") {
+const handleNthResponse = async (n, responses) => {
+	console.log("Handling response: " + n);
+	// TODO: we shouldnt need to check for having responses because we are in the test-state
+	if (n >= 0) {
+		const res = responses[n];
+		if (res.interval != 0) {
+			responseHandler(getCurrentCard(), res.interval, res.signal.toString());
+		} else {
+			await responseHandler(getCurrentCard(), res.interval, res.signal.toString());
+		}
 		stepToNext();
-		return;
+	}
+};
+
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const handleDigitResponse = (digit) => {
+	var responses = getCurrentCard().algorithm(getCurrentCard().history);
+	var n = Math.min(digit - 1, responses.length - 1);
+	handleNthResponse(n, responses);
+};
+const digitsCodeMap = Object.fromEntries(digits.map((digit) => ["Digit" + digit, () => handleDigitResponse(digit)]));
+
+const letters = ["KeyH", "KeyJ", "KeyK", "KeyL"];
+const handleLetterResponse = (letter) => {
+	var responses = getCurrentCard().algorithm(getCurrentCard().history);
+	var n = Math.min(letters.indexOf(letter), responses.length - 1);
+	handleNthResponse(n, responses);
+};
+const lettersCodeMap = Object.fromEntries(letters.map((letter) => [letter, () => handleLetterResponse(letter)]));
+
+const testCodeMap = {
+	...digitsCodeMap,
+	...lettersCodeMap,
+	...reviewAndTestCodeMap,
+};
+
+const statusCodeMaps = { review: reviewCodeMap, test: testCodeMap };
+
+// note: changing these requires reloading Roam because of the keylistener
+export const processKey = (e) => {
+	// if we are editing, dont process
+	if (document.activeElement.type === "textarea") return;
+
+	const statusCodeMap = statusCodeMaps[roamsr.state.status];
+	if (statusCodeMap) {
+		const func = statusCodeMap[e.code];
+		if (func) {
+			func(e);
+		}
 	}
 
-	if (e.code == "KeyD" && e.altKey) {
-		endSession();
-		return;
-	}
+	// TODO: this should not be necessary anymore because we have status, maybe race-condition?
+	// !location.href.includes(getCurrentCard().uid)
 };
 
 export const processKeyAlways = (e) => {
