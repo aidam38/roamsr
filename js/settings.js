9f9dc7f2a4e90ac29ad805049950e59f877a72c0
luccr1 <lucca.hellriegel@gmx.net>
2021-03-21 19:38:02 +0100

    format settings.js

--- js/settings.js
+++ js/settings.js
@@ -6,14 +6,14 @@ window.roamsrUserSettings = {};
 // Type: String
 roamsrUserSettings.mainTag = "sr";
 
-// Tag used to flag cards. 
+// Tag used to flag cards.
 // Cardblocks with this tag won't get shown in review (meant for rewrite)
 // Type: String
 roamsrUserSettings.flagTag = "f";
 
 // Cloze deletion style
 // Type: String
-// Valid values: "highlight" (^^cloze^^), 
+// Valid values: "highlight" (^^cloze^^),
 //               "block-ref" (using "Create as block below.")
 roamsrUserSettings.clozeStyle = "highlight";
 
@@ -29,27 +29,27 @@ roamsrUserSettings.defaultDeck.reviewLimit = 50;
 // Default scheduler
 // Type: String or function (see custom algorithms at the end)
 // Valid values: "anki", function (without the `()`)
-roamsrUserSettings.defaultDeck.scheduler = "anki"
+roamsrUserSettings.defaultDeck.scheduler = "anki";
 
 // Default scheduler config
-// For more info on Anki, see: 
+// For more info on Anki, see:
 // https://faqs.ankiweb.net/what-spaced-repetition-algorithm.html
 roamsrUserSettings.defaultDeck.config = {
-  defaultFactor: 2.5,
-  firstFewIntervals: [1, 6],
-  factorModifier: 0.15,
-  easeBonus: 1.3,
-  hardFactor: 1.2,
-  minFactor: 1.3,
-  jitterPercentage: 0.05,
-  maxInterval: 50 * 365,
-  responseTexts: ["Again.", "Hard.", "Good.", "Easy."]
+	defaultFactor: 2.5,
+	firstFewIntervals: [1, 6],
+	factorModifier: 0.15,
+	easeBonus: 1.3,
+	hardFactor: 1.2,
+	minFactor: 1.3,
+	jitterPercentage: 0.05,
+	maxInterval: 50 * 365,
+	responseTexts: ["Again.", "Hard.", "Good.", "Easy."],
 };
 
 /* ====== CUSTOM DECKS ====== */
 // Don't forget to add your decks to the Array at the end of this section
 
-/* MY DECK (example) */ 
+/* MY DECK (example) */
 var myDeck = {};
 // Deck's main tag (if a card references this page, it's in this deck)
 // If a card is in multiple decks, the most recent one is picked
@@ -64,36 +64,36 @@ myDeck.newCardLimit = 10;
 myDeck.reviewLimit = 30;
 
 // Custom scheduler
-// Should be a function that returns a function, 
+// Should be a function that returns a function,
 //  which takes the history of a card and the current signal as input
 //  and outputs the set of responses
 // See: https://roamresearch.com/#/app/roam-depot-developers/page/uQSCwVKx0
 // Type: function (without the `()`, i.e. not the return value, but the function itself)
 myDeck.scheduler = (config) => {
-  // Configure your scheduler using config...
-  
-  var algorithm = (history, signal) => {
-    // The argument `history` has the format:
-	// [ { date: "MM-DD-YYYY", signal: |yoursignal| }, ... ] (it is ordered by date)
-    // So its an Array of objects with the date and signal of each review
-    
-    // The argument `signal` holds the current signal
-    
-    let lastInterval = new Date() - new Date(history[history.length - 1].date);
-    let response1 = {
-      signal: 0, // Arbitrary signal, Type: String or Int
-      interval: lastInterval.getDate(), // Next interval, Type: Int
-      responseText: "Same as last time.", // Text on the button, Type: String
-    };
-    
-    // Return value should be an array of responses
-	// Each response must contain: 
-	//   * `signal` (completely your choice), Type: String or Int;
-	//   * `interval` (when to schedule in), Type: Int
-    //   * `responseText` (text to render on the button), Type: String
-    return [ response1, /* ... other responses */ ]
-  }
-  return algorithm;  
+	// Configure your scheduler using config...
+
+	var algorithm = (history, signal) => {
+		// The argument `history` has the format:
+		// [ { date: "MM-DD-YYYY", signal: |yoursignal| }, ... ] (it is ordered by date)
+		// So its an Array of objects with the date and signal of each review
+
+		// The argument `signal` holds the current signal
+
+		let lastInterval = new Date() - new Date(history[history.length - 1].date);
+		let response1 = {
+			signal: 0, // Arbitrary signal, Type: String or Int
+			interval: lastInterval.getDate(), // Next interval, Type: Int
+			responseText: "Same as last time.", // Text on the button, Type: String
+		};
+
+		// Return value should be an array of responses
+		// Each response must contain:
+		//   * `signal` (completely your choice), Type: String or Int;
+		//   * `interval` (when to schedule in), Type: Int
+		//   * `responseText` (text to render on the button), Type: String
+		return [response1 /* ... other responses */];
+	};
+	return algorithm;
 };
 
 // Whatever parameters you want your scheduler to have
@@ -102,7 +102,6 @@ myDeck.config = {};
 
 /* ARRAY OF CUSTOM DECKS */
 // Type: Array of Objects
-roamsrUserSettings.defaultDeck.customDecks = [ myDeck, /* ... other decks */ ];
+roamsrUserSettings.defaultDeck.customDecks = [myDeck /* ... other decks */];
 
-
-console.log("🗃️ Loaded roam/sr settings.")
+console.log("🗃️ Loaded roam/sr settings.");
